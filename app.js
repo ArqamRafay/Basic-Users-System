96df0c6571e1d8547aeefb33fcb55b14dbde096f
ArqamRafay <getarqamnow@gmail.com>
2022-06-25 12:04:46 +0500

    working not complete

--- app.js
+++ app.js
@@ -4,30 +4,51 @@ const app = express()
 const jwt = require("jsonwebtoken")
 const expressJWT = require("express-jwt")
 const bodyParser = require("body-parser")
+const key = require('./config/auth.config')
 const PORT = process.env.PORT || 8081;
+
+const db = require("./models");
+// db.sequelize.sync();
+db.sequelize.sync().then(() => {
+    console.log("All models were synchronized successfully.");
+    console.log("Drop and re-sync db.");
+}, { logging: false });
+let secret = 'some_secret';
 
 app.use(cors());
 app.options('*', cors)
 app.use(bodyParser.json({ limit: '10mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
-let secret = 'somesecretkey'
-
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 
-/* CODE IN BETWEEN */
 //SECRET FOR JSON WEB TOKEN
-
-// app.use(expressJWT({secret:secret, algorithm:['HS256']})).unless({
-//     path: [
-//         '/token'
-//     ]
-// })
+/* ALLOW PATHS WITHOUT TOKEN AUTHENTICATION */
+app.use(expressJWT({ secret: secret, algorithms: ['HS256'] })
+    .unless(
+        {
+            path: [
+                '/token/sign',
+                '/encryptedData'
+            ]
+        }
+    ));
 
 //Root Open API
 app.get('/', (req, res) => {
     res.json("Welcome to undefined world");
 });
+
+/* CREATE TOKEN FOR USE */
+app.get('/token/sign', (req, res) => {
+    var userData = {
+        "name": "Muhammad Bilal",
+        "id": "4321"
+    }
+    let token = jwt.sign(userData, secret, { expiresIn: '150s' })
+    res.status(200).json({ "token": token });
+})
+
+
---
+++ config/connection.js
@@ -0,0 +1,32 @@
+const development = {
+    database: 'testDB',
+    username: 'postgres',
+    password: 'admin',
+    host: 'localhost',
+    dialect: 'postgres' ,
+    
+    // dialect: 'sqlite' || 'mysql' || 'postgres',
+  };
+  
+const testing = {
+    database: 'testdb',
+    username: 'root',
+    password: '',
+    host: 'localhost',
+    dialect: 'mysql',
+  };
+  
+  const production = {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'sqlite' || 'mysql' || 'postgres',
+  };
+  
+  module.exports = {
+    development,
+    testing,
+    production,
+  };
+  
